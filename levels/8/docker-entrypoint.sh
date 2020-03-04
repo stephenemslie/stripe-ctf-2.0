1bfd6c03be3842f9e45ef51ca4c2cbdd9a86a92e
Stephen Emslie <stephenemslie@gmail.com>
2020-03-04 10:40:28 +0200

    Generate password with python
    The password for this level needs to be a series of 12 numbers, so the
    usual method of piping /dev/urandom to base64 doesn't work.
    
    This uses the secrets module to generate integers in the most secure
    method possible for the platform.

--- levels/8/docker-entrypoint.sh
+++ levels/8/docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+if [ ! -f $PW_FILE ]; then
+    python -c "import secrets; import sys; sys.stdout.write(''.join([str(secrets.randbelow(10)) for i in range(12)]))" > $PW_FILE
+fi
+
 if [ "$1" = 'serve' ]; then
-    if [ ! -f password.txt ]; then
-        base64 /dev/urandom | head -c 10 > password.txt
-    fi
-    PASSWORD=`cat password.txt`
-    exec ./password_db_launcher $PASSWORD 0.0.0.0:4000
+    PASSWORD=`cat $PW_FILE`
+    exec ./password_db_launcher "$PASSWORD" 0.0.0.0:4000
 fi
 
 exec "$@"
