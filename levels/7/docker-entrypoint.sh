22e451995393818178158ff3e6ff68e31415a4ef
Stephen Emslie <stephenemslie@gmail.com>
2020-03-04 10:39:15 +0200

    Store password in file, and init separately

--- levels/7/docker-entrypoint.sh
+++ levels/7/docker-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+if [ ! -f $PW_FILE ]; then
+    base64 /dev/urandom | head -c 10 > $PW_FILE
+fi
+
+if [ ! -f ./wafflecopter.db ]; then
+    python initialize_db.py `echo $PW_FILE`
+fi
+
 if [ "$1" = 'serve' ]; then
-    if [ ! -f ./wafflecopter.db ]; then
-        python initialize_db.py `base64 /dev/urandom | head -c 10`
-    fi
     exec python wafflecopter.py
 fi
 
