b8b42949e6f25419d37a4a17dcc5de563faa05fc
Stephen Emslie <stephenemslie@gmail.com>
2020-03-04 10:22:02 +0200

    Set data_dir via environment variable

--- levels/3/secretvault.py
+++ levels/3/secretvault.py
@@ -26,7 +26,7 @@ import sys
 from werkzeug import debug
 
 # Generate test data when running locally
-data_dir = os.path.join(os.path.dirname(__file__), 'data')
+data_dir = os.environ['LEVEL3_DATA_DIR']
 if not os.path.exists(data_dir):
     import generate_data
     os.mkdir(data_dir)
