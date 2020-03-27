f5d290a043caf67b05129cea3aa8a9d5c7e671b9
Stephen Emslie <stephenemslie@gmail.com>
2020-03-27 14:16:09 +0200

    Added watch commands for go and node

--- ctfproxy/docker-entrypoint.sh
+++ ctfproxy/docker-entrypoint.sh
@@ -4,4 +4,12 @@ if [ "$1" = 'serve' ]; then
     exec go run main.go
 fi
 
+if [ "$1" = 'gowatch' ]; then
+    exec CompileDaemon --build="go build" --command=./ctfproxy
+fi
+
+if [ "$1" = 'npmwatch' ]; then
+    exec npm run watch
+fi
+
 exec "$@"
