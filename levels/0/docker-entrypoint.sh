a0b731b1b7d0cdb2dc791d08b7cddf2ba9551dd2
Stephen Emslie <stephenemslie@gmail.com>
2020-04-02 22:57:03 +0200

    Reset database on level0 start

--- levels/0/docker-entrypoint.sh
+++ levels/0/docker-entrypoint.sh
@@ -4,11 +4,9 @@ if [ ! -f $PW_FILE ]; then
     base64 /dev/urandom | head -c 10 > $PW_FILE
 fi
 
-if [ ! -f level00.db ]; then
-    node ctf-install.js `cat $PW_FILE`
-fi
-
 if [ "$1" = 'serve' ]; then
+    rm level00.db
+    node ctf-install.js `cat $PW_FILE`
     exec node level00.js
 fi
 
