3c807272daae13604117d3e5d87f9a42e09c0530
Stephen Emslie <stephenemslie@gmail.com>
2020-04-03 11:40:19 +0200

    Guarantee single and double quotes in generated password
    This is important for level 6's challenge.

--- levels/6/server/docker-entrypoint.sh
+++ levels/6/server/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f $PW_FILE ]; then
-    base64 /dev/urandom | head -c 10 > $PW_FILE
+    cat /dev/urandom | tr -dc "A-Z0-9\"'" | fold -w 16 | grep "'" | grep "\"" | head -n 1 | tr -d '\n' > $PW_FILE
 fi
 
 if [ "$1" = 'serve' ]; then
