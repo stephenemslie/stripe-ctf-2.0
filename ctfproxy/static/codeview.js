3ecbc0a001f04e580c5b280ba08dbb2b622e48d6
Stephen Emslie <stephenemslie@gmail.com>
2020-04-06 10:57:11 +0200

    Added hint toggling

--- ctfproxy/static/codeview.js
+++ ctfproxy/static/codeview.js
@@ -10,6 +10,7 @@ function CodeView(props) {
   const { index } = props;
   const [level, setLevel] = React.useState(null);
   const [activeSource, setActiveSource] = React.useState(null);
+  const [hints, setHints] = React.useState(false);
   React.useEffect(() => {
     fetchLevel(index).then(level => {
       setLevel(level);
@@ -19,26 +20,46 @@ function CodeView(props) {
   React.useEffect(() => {
     Prism.highlightAll();
   }, [activeSource]);
+
+  if (!(level && activeSource)) {
+    return <div />;
+  }
+
   return level && activeSource ? (
-    <div>
-      <div class="bg-prism-bg rounded-t-lg p-4 mt-10">
-        {Object.entries(level.Source).map(([key, source]) => {
-          const active = source.Name == activeSource.Name;
-          return (
-            <button
-              className={`rounded py-1 px-2 mr-2 text-sm focus:outline-none ${
-                active
-                  ? "bg-gray-700 text-gray-300"
-                  : "text-gray-500 hover:text-indigo-600"
-              }`}
-              onClick={() => {
-                setActiveSource(source);
-              }}
-            >
-              {source.Basename}
-            </button>
-          );
-        })}
+    <div className={hints ? "showhints" : ""}>
+      <div class="bg-prism-bg rounded-t-lg p-4 mt-10 flex flex-row">
+        <div class=" flex-grow">
+          {Object.entries(level.Source).map(([key, source]) => {
+            const active = source.Name == activeSource.Name;
+            return (
+              <button
+                className={`rounded py-1 px-2 mr-2 text-sm focus:outline-none ${
+                  active
+                    ? "bg-gray-600 text-gray-300"
+                    : "text-gray-500 hover:bg-gray-700"
+                }`}
+                onClick={() => {
+                  console.log(source);
+                  setActiveSource(source);
+                }}
+              >
+                {source.Basename}
+              </button>
+            );
+          })}
+        </div>
+        <div class="w-32">
+          <button
+            className={`rounded py-1 px-2 mr-2 text-sm focus:outline-none float-right hover:bg-gray-700 ${
+              hints ? "bg-gray-600 text-gray-300" : "text-gray-500"
+            }`}
+            onClick={() => {
+              setHints(!hints);
+            }}
+          >
+            hints {hints ? "on" : "off"}
+          </button>
+        </div>
       </div>
       <pre class="rounded-b-lg">
         <code className={"language-" + activeSource.Language}>
@@ -46,7 +67,5 @@ function CodeView(props) {
         </code>
       </pre>
     </div>
-  ) : (
-    <div />
   );
 }
