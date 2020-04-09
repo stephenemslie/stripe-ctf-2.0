bbd298d783302b2f1677187ba9d9259ba17aa457
Stephen Emslie <stephenemslie@gmail.com>
2020-04-10 01:26:18 +0200

    Use inotify to monitor for a reset signal
    This is a way for one container to send a signal to another
    without mounting the docker socket into the former. This then allows the
    latter to reset it's state, which is useful because some solutions put
    containers into an unstable state.
    
    inotify watches a directory for changes, and resets application state
    and the process when the reset pattern matches.

--- levels/4/server/docker-entrypoint.sh
+++ levels/4/server/docker-entrypoint.sh
@@ -5,7 +5,22 @@ if [ ! -f $PW_FILE ]; then
 fi
 
 if [ "$1" = 'serve' ]; then
-    exec ruby srv.rb
+    ruby srv.rb&
+    PID=$!
+    WATCH_DIR=`dirname $RESET_FILE`
+    WATCH_NAME=`basename $RESET_FILE`
+    inotifywait -m -e CREATE $WATCH_DIR |
+        while read path action file; do
+            if [[ "$WATCH_NAME" = "$file" ]]; then
+                echo "RESETTING"
+                kill $PID
+                rm $DB_FILE
+                rm $RESET_FILE
+                sleep 3
+                ruby srv.rb&
+                PID=$!
+            fi
+        done
 fi
 
 exec "$@"
