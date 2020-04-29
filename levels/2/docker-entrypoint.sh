0d1564db969153e098fe6c4107adc294c8a9cd35
Stephen Emslie <stephenemslie@gmail.com>
2020-04-29 09:49:43 +0200

    Import password from secrets manager on start

--- levels/2/docker-entrypoint.sh
+++ levels/2/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-mkdir -p /usr/src/app/uploads
-ln -s $PW_FILE password.txt
-
-if [ ! -f $PW_FILE ]; then
-  base64 /dev/urandom | head -c 10 > $PW_FILE
+if [ -n "$GSM_PASSWORD_KEY" ]; then
+    export LEVEL2_PW=`php get_secret.php $GSM_PASSWORD_KEY`
 fi
+
+mkdir -p /usr/src/app/uploads
+echo $LEVEL2_PW > password.txt
 
 if [ "$1" == 'serve' ]; then
   /usr/sbin/sshd
