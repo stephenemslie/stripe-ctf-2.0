6715cb3434eb5cb6cef12c80ed095fadd53d22a4
Stephen Emslie <stephenemslie@gmail.com>
2020-05-04 19:56:15 +0200

    Added cloud scheduler trigger to level4 browser

--- terraform/main.tf
+++ terraform/main.tf
@@ -286,6 +286,45 @@ resource "google_cloud_run_service_iam_member" "level4_browser_invoker" {
   member   = "serviceAccount:${module.level4_browser.service.template[0].spec[0].service_account_name}"
 }
 
+# Cloud scheduler requires an app engine application to be present in the region
+
+resource "google_app_engine_application" "app" {
+  project     = "stripe-ctf-demo"
+  location_id = "us-central"
+}
+
+resource "google_cloud_scheduler_job" "level4_browser" {
+  name             = "level4-browser-trigger"
+  schedule         = "* * * * *"
+  time_zone        = "America/New_York"
+  attempt_deadline = "320s"
+  depends_on       = [google_app_engine_application.app]
+
+  http_target {
+    http_method = "POST"
+    uri         = "${module.level4_browser.service.status[0].url}/"
+
+    oidc_token {
+      service_account_email = google_service_account.level4_browser_scheduler.email
+    }
+
+  }
+
+}
+
+resource "google_service_account" "level4_browser_scheduler" {
+  account_id = "level4-browser-scheduler"
+}
+
+resource "google_cloud_run_service_iam_member" "level4_browser_sheduler_invoker" {
+  location = module.level4_browser.service.location
+  project  = module.level4_browser.service.project
+  service  = module.level4_browser.service.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.level4_browser_scheduler.email}"
+}
+
+
 module "level5" {
   source                = "./modules/level"
   name                  = "level5"
