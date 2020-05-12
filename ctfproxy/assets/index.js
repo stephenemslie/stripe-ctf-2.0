2454bc8985547eb809e3fc716719c3f62b3a257f
Stephen Emslie <stephenemslie@gmail.com>
2020-05-12 15:48:24 +0200

    Added non-default prism components to webpack build

--- ctfproxy/assets/index.js
+++ ctfproxy/assets/index.js
@@ -4,6 +4,11 @@ import ReactDOM from "react-dom";
 import stylesCSS from "./styles.css";
 import prismCSS from "./prism-tailwind.css";
 import "prismjs";
+import "prismjs/components/prism-ruby";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-markup-templating";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-php";
 
 export const initCodeView = (props, element) => {
   ReactDOM.render(React.createElement(CodeView, props, null), element);
