0d140d283c945f9978143b2a7acf72bf5673630e
Stephen Emslie <stephenemslie@gmail.com>
2020-05-12 15:09:52 +0200

    Added prism to webpack build

--- ctfproxy/assets/index.js
+++ ctfproxy/assets/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import stylesCSS from "./styles.css";
 import prismCSS from "./prism-tailwind.css";
+import "prismjs";
 
 export const initCodeView = (props, element) => {
   ReactDOM.render(React.createElement(CodeView, props, null), element);
