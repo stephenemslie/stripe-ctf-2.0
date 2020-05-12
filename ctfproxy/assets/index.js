f11403d419a7c57daf1714c44d1b89a2070ae4d2
Stephen Emslie <stephenemslie@gmail.com>
2020-05-12 15:03:56 +0200

    Include css into webpack build

--- ctfproxy/assets/index.js
+++ ctfproxy/assets/index.js
@@ -1,6 +1,8 @@
 import CodeView from "./codeview.jsx";
 import React from "react";
 import ReactDOM from "react-dom";
+import stylesCSS from "./styles.css";
+import prismCSS from "./prism-tailwind.css";
 
 export const initCodeView = (props, element) => {
   ReactDOM.render(React.createElement(CodeView, props, null), element);
