a2fbfdc3e04f3267b11c7b8c012fd6de9f620968
Stephen Emslie <stephenemslie@gmail.com>
2020-05-12 16:20:42 +0200

    Added npmbuild entrypoint

--- ctfproxy/docker-entrypoint.sh
+++ ctfproxy/docker-entrypoint.sh
@@ -8,6 +8,10 @@ if [ "$1" = 'gowatch' ]; then
     exec CompileDaemon --build="go build" --include="*.html" --command=./ctfproxy
 fi
 
+if [ "$1" = 'npmbuild' ]; then
+    exec npm run build
+fi
+
 if [ "$1" = 'npmwatch' ]; then
     exec npm run watch
 fi
